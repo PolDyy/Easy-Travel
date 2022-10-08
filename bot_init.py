dbe08fc5eab2ca0066f5ddc0e25be4a77d513873
PolDyy <94655918+PolDyy@users.noreply.github.com>
2022-10-08 13:11:28 +0700

    Update bot_init.py

--- bot_init.py
+++ bot_init.py
@@ -3,7 +3,7 @@ from telebot import StateMemoryStorage
 from sql_requests import init_db
 
 
-token = '5606339525:AAFLABtOiFtu98-my4ACAJ9CT07yLBvjsyE'
+token = ''
 storage = StateMemoryStorage()
 bot = telebot.TeleBot(token, state_storage=storage)
 
